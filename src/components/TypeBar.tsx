951ac065390a398b37496a5a98d3c538bc5ddd02
Sergey Kravchenko <sergey.ose.pyatigosk@gmail.com>
2024-07-18 17:03:29 +0300

    feat: add shop page

---
+++ src/components/TypeBar.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+import { observer } from 'mobx-react-lite'
+import { useContext } from 'react'
+import { Context } from '../main.tsx'
+
+export const TypeBar = observer(() => {
+	const { device } = useContext(Context)
+
+	return (
+		<Root>
+			{device.brands.map(type => (
+				<Li key={type.id} onClick={() => device.setSelectedType(type)}>
+					{type.name}
+				</Li>
+			))}
+		</Root>
+	)
+})
+
+const Root = styled.ul`
+	max-width: 1440px;
+	margin: 0 auto;
+	padding: 0 24px;
+`
+
+const Li = styled.li`
+	padding: 8px 24px;
+	cursor: pointer;
+`
--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,9 +1,40 @@
 import { Header } from '../components/Header.tsx'
+import styled from 'styled-components'
+import { observer } from 'mobx-react-lite'
+import { TypeBar } from '../components/TypeBar.tsx'
+import { BrandBar } from '../components/BrandBar.tsx'
+import { DeviceList } from '../components/DeviceList.tsx'
 
-export const ShopPage = () => {
+export const ShopPage = observer(() => {
 	return (
-		<div>
-			<Header />
-		</div>
+		<Root>
+			<Container>
+				<Header />
+				<Main>
+					<div>
+						<TypeBar />
+						<BrandBar />
+					</div>
+					<DeviceList />
+				</Main>
+			</Container>
+		</Root>
 	)
-}
+})
+
+const Root = styled.div`
+	padding: 0 24px;
+`
+
+const Container = styled.div`
+	max-width: 1440px;
+	margin: 0 auto;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+`
+
+const Main = styled.div`
+	padding: 0 24px;
+	display: flex;
+`
